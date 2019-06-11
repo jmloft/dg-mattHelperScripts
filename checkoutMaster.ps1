4ea8f69b01e18bdabcde0f0c4f4489f1cfea5cc2
Matt Loft (TMNA) <matt.loft@toyota.com>
2019-06-11 11:51:20 -0500

    make checkout master work for all repos

--- checkoutMaster.ps1
+++ checkoutMaster.ps1
@@ -1,9 +1,85 @@
 cd ..
-cd digital-garage-library
+cd dg-commons-library
+git checkout master
+git pull
+cd ..
+cd dg-components
 git checkout master
 git pull
 cd ..
 cd dg-spas
+git checkout master
+git pull
+cd ..
+cd dg-estimate-service
+git checkout master
+git pull
+cd ..
+cd dg-infrastructure
+git checkout master
+git pull
+cd ..
+cd dg-mattHelperScripts
+git checkout master
+git pull
+cd ..
+cd dg-notification-service
+git checkout master
+git pull
+cd ..
+cd dg-offer-notify-service
+git checkout master
+git pull
+cd ..
+cd dg-routing
+git checkout master
+git pull
+cd ..
+cd dg-search-notify-service
+git checkout master
+git pull
+cd ..
+cd dg-vin-notify-service
+git checkout master
+git pull
+cd ..
+cd digital-garage-lead-service
+git checkout master
+git pull
+cd ..
+cd digital-garage-library
+git checkout master
+git pull
+cd ..
+cd digital-garage-loader-library
+git checkout master
+git pull
+cd ..
+cd digital-garage-service
+git checkout master
+git pull
+cd ..
+cd digital-garage-si-service
+git checkout master
+git pull
+cd ..
+cd efc-model-service
+git checkout master
+git pull
+cd ..
+cd qa-automation
+git checkout master
+git pull
+cd ..
+cd t4-web-app
+git checkout master
+git pull
+cd ..
+cd websocket-library
+git checkout master
+git pull
+cd ..
+cd websocket-service
 git checkout master
 git pull
 cd ..
