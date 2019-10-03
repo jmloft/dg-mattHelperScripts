6df649a55f2957c44380ce6f4a3afacb4b8cc3c6
Matt Loft (TMNA) <matt.loft@toyota.com>
2019-10-03 13:31:17 -0500

    check out a branch from all repos

---
+++ checkoutABranch.ps1
@@ -0,0 +1,83 @@
+$branchName = 'SDG-3297-AddStageJenkinsFile'
+cd ..
+cd ./dg-components
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./dg-spas
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./dg-estimate-service
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./dg-notification-service
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./dg-offer-notify-service
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./dg-routing
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./dg-search-notify-service
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./dg-vin-notify-service
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./digital-garage-lead-service
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./digital-garage-library
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./digital-garage-loader-library
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./digital-garage-service
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./digital-garage-si-service
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./efc-model-service
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./websocket-library
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./websocket-service
+git add .
+git commit -m 'add stage folder and jenkinsfile'
+git push '--set-upstream' 'origin' $branchName
+cd ..
+cd ./dg-mattHelperScripts
